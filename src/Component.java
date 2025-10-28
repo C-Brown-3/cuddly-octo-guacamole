f3ae09989d1315385c11afcb32a779250f32fc27
nielsenc <nielsenc@rose-hulman.edu>
2025-10-28 14:48:16 -0400

    filled main class

--- src/Component.java
+++ src/Component.java
@@ -1,9 +1,37 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
-public class Component {
+import javax.swing.JComponent;
+import javax.swing.Timer;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+public class Component extends JComponent{
+	
+	Timer timer;  
+
+	public Component() {
+		
+		timer = new Timer(30, e -> {
+	          
+	          repaint();
+	      });
 	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D) g;
+		this.drawSquare(g2d);
+	}
+	
+	private void drawSquare(Graphics2D g2d) {
+		Rectangle rect=new Rectangle(10,10,10,10);
+		g2d.draw(rect);
+	}
+
+	
 
 }
--- src/Viewer.java
+++ src/Viewer.java
@@ -1,9 +1,17 @@
+import javax.swing.JFrame;
 
 public class Viewer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame("Viewer");
+		frame.setSize(600, 400);
+		//frame.pack(); // 1) compute preferred size
+		frame.setLocationRelativeTo(null); // 2) center on screen
+		
+		frame.add(new Component());
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 
 }
