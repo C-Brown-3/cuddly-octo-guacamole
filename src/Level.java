2d35cae8db3efe6e1c562f786b24b345ca3b9a9a
rhit-goadtw <goadtw@rose-hulman.edu>
2025-10-31 14:22:38 -0400

    Fixed level to be more accessible by enemy and Player

--- src/Level.java
+++ src/Level.java
@@ -1,28 +1,30 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+
 public class Level {
-	private int levelNum=1;//int to determine what level we are on
 	private Tile ground;
 	private Tile platform;
 	private Tile spawn=new Tile(0,744,64,-32);
 	private Tile stair;
-	public Level(Graphics2D g2) {
-		levelOne(g2);
+	private int levelNum=1;//int to determine what level we are on
+	
+	public Level() {
+		levelOne();
 	}
 	
-	public Level(int levelNum,Graphics2D g2) {
+	public Level(int levelNum) {
 		this.levelNum=levelNum;
 		if(levelNum==1){//checks the provided level number to initialize that stage
-			levelOne(g2);
+			levelOne();
 		}
 	}
-	public void levelOne(Graphics2D g2) { //Creates the first level of the game
-		new Tile(0,744,3200,160,Color.GREEN).draw(g2); //Creates the floor in which the size is a multiple of 32 for scaling with character
-		new Tile(330,394,32*12,64,Color.DARK_GRAY).draw(g2);//creates a platform with scale to the character
-		for(int k =0;k<=11;k++) { //creates a set of stairs that connects to the platform
-			new Tile(714+(32*k),394+(k*32),32*4,352-(k*32),Color.BLUE).draw(g2);
-		}
+	public void levelOne() { //Creates the first level of the game
+		ground = new Tile(0,744,3200,160,Color.GREEN); //Creates the floor in which the size is a multiple of 32 for scaling with character
+		platform = new Tile(330,394,32*12,64,Color.DARK_GRAY);//creates a platform with scale to the character
+//		for(int k =0;k<=11;k++) { //creates a set of stairs that connects to the platform
+//			stair = new Tile(714+(32*k),394+(k*32),32*4,352-(k*32),Color.BLUE);
+//		}
 		//Scaling is based on size 32X32
 	}
 	public Tile getTile(String name) {
@@ -35,10 +37,17 @@ public class Level {
 		if(name.equals("platform")) {
 			return platform;
 		}
-		if(name.equals("stair")) {
-			return stair;
-		}
+//		if(name.equals("stair")) {
+//			return stair;
+//		}
 		return new Tile(0,744,64,-32);
-	}	
-	
+	}
+	public void levelDraw(Graphics2D g2) {
+		ground.draw(g2);
+		platform.draw(g2);
+//		for(int k =0;k<=11;k++) { //creates a set of stairs that connects to the platform
+//			Tile stair = new Tile(714+(32*k),394+(k*32),32*4,352-(k*32),Color.BLUE);
+//			stair.draw(g2);
+//		}
+	}
 }
