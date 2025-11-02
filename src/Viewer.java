933cc685c29e1c6b4271b62bf1e6c9d58d49376f
C-Brown-3 <brownc3@rose-hulman.edu>
2025-11-02 17:12:47 -0500

    Javadoc for Viewer Class

--- src/Viewer.java
+++ src/Viewer.java
@@ -2,13 +2,12 @@ import javax.swing.JFrame;
 
 
 public class Viewer{
-	
-	
-	
-	
-	public Viewer() {
-		
-	}
+	/**
+	 * The Viewer Class is the entry point for the program which instances Component and attaches it to a JFrame.
+	 *	It is responsible for all window management decisions and logic, such as the title of the program. 
+	 *
+	 */
+
 	public static void main(String[] args) {
 		Component comp=new Component();
 		JFrame frame = new JFrame("Viewer");
