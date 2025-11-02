d64f2e3dbc724e52c1feb198c03aff86ad34aae6
rhit-goadtw <goadtw@rose-hulman.edu>
2025-11-02 17:21:41 -0500

    Javadocs for level and tile

--- src/Trevor/Level.java
+++ src/Trevor/Level.java
@@ -3,7 +3,11 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
 
-
+/**
+ * Class: Level
+ * @author Trevor Goad
+ * <br>Purpose: Used to create the set level design
+ */
 public class Level {
 	private Tile ground;
 	private Tile platform1;
@@ -11,17 +15,25 @@ public class Level {
 	private Tile platform3;
 	//private Tile stair;
 	private int levelNum=1;//int to determine what level we are on
-	
+	/**
+	 * initializes the base level
+	 */
 	public Level() {
 		levelOne(); //Calls level 1
 	}
-	
+	/**
+	 * 
+	 * @param levelNum
+	 */
 	public Level(int levelNum) {
 		this.levelNum=levelNum;
 		if(levelNum==1){//checks the provided level number to initialize that stage
 			levelOne();
 		}
 	}
+	/**
+	 * creates the tiles for the first level
+	 */
 	public void levelOne() { //Creates the first level of the game
 		ground = new Tile(0,744,3200,160,Color.GREEN); //Creates the floor in which the size is a multiple of 32 for scaling with character
 		platform1 = new Tile(330+32*4,394-32*2,32*12,32,Color.DARK_GRAY);//creates a platform with scale to the character
@@ -32,12 +44,24 @@ public class Level {
 //		}
 		//Scaling is based on size 32X32
 	}
+	/**
+	 * 
+	 * @return spawnpoint x
+	 */
 	public int spawnX() {
 		return 64*2;
 	}
+	/**
+	 * 
+	 * @return spawnpoint y
+	 */
 	public int spawnY() {
 		return 650;
 	}
+	/**
+	 * creates an arraylist of the tiles for the other classes to use
+	 * @return arraylist of tiles
+	 */
 	public ArrayList<Tile> getTiles() {
 		ArrayList<Tile> tiles = new ArrayList<>();
 		tiles.add(ground);
@@ -46,6 +70,10 @@ public class Level {
 		tiles.add(platform3);
 		return tiles;
 	}
+	/**
+	 * draws the tiles on the frame
+	 * @param g2
+	 */
 	public void draw(Graphics2D g2) {
 		ground.draw(g2);
 		platform1.draw(g2);
